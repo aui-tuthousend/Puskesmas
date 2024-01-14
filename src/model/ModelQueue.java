56bed4b6a9a1947b4fc060f8506498e55ea6b0bd
aui_tuthousend <mraihan286@gmail.com>
2024-01-14 17:56:02 +0700

    add model queue

---
+++ src/model/ModelQueue.java
@@ -0,0 +1,38 @@
+package model;
+
+import com.google.gson.reflect.TypeToken;
+import modelGSON.ModelGSON;
+import node.Schedule;
+
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class ModelQueue {
+    private ArrayList<node.Queue> queues;
+    ModelGSON modelGSON;
+
+    public ModelQueue(){
+        modelGSON = new ModelGSON("src/database/queue.json");
+
+        queues = modelGSON.readFromFile(new TypeToken<ArrayList<Queue>>() {
+        }.getType());
+        if (queues == null){
+            this.queues = new ArrayList<>();
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("\nShutting down. Saving data to JSON file...");
+            modelGSON.writeToFile(queues);
+        }));
+    }
+
+    public void addQueue(node.Queue queue){
+        this.queues.add(queue);
+    }
+
+    public void showAllQueues(){
+        for (node.Queue queue: queues){
+            queue.viewQueue();
+        }
+    }
+}
--- src/node/Queue.java
+++ src/node/Queue.java
@@ -3,14 +3,17 @@ package node;
 public class Queue {
     public int index;
     public Pasien pasien;
+    public String poli;
 
-    public Queue(int index, Pasien pasien) {
+    public Queue(int index, Pasien pasien, String poli) {
         this.index = index;
         this.pasien = pasien;
+        this.poli = poli;
     }
 
     public void viewQueue(){
-        System.out.println("Antrean ke  : "+index);
+        System.out.println("Poli        : "+poli);
+        System.out.println("No Antrean  : "+index);
         System.out.println("Nama pasien : "+pasien.namaPasien);
     }
 }
