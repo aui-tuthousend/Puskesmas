b833f895f0d8532723782f5335f8842f2ac373a0
aui_tuthousend <mraihan286@gmail.com>
2024-01-15 17:32:26 +0700

    add action listener to each ba'en

--- src/view/HomeAdmin.java
+++ src/view/HomeAdmin.java
@@ -1,6 +1,8 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +16,7 @@ public class HomeAdmin extends JFrame {
         setLayout(null);
         setResizable(false);
         components();
+        actionHandler();
         setVisible(true);
     }
 
@@ -45,5 +48,35 @@ public class HomeAdmin extends JFrame {
         HomePage homePage = new HomePage();
         this.setVisible(false);
         homePage.setVisible(true);
+    }
+
+    public void actionHandler(){
+        buttons.get(0).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        buttons.get(1).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        buttons.get(2).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        buttons.get(3).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
