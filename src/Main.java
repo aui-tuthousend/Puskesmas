b43744dd00351bba606bea6e726e61038afde16b
aui_tuthousend <mraihan286@gmail.com>
2024-01-25 20:56:25 +0700

    fix

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ import view.HomePage;
 import javax.swing.*;
 
 public class Main {
+    public Main(){
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("\nShutting down. Saving data to JSON file...")));
+    }
     public static void main(String[] args) {
+        Main main = new Main();
         try {
             SwingUtilities.invokeLater(HomePage::new);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Error Gagal Membuat GUI: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
-
-//        System.out.println("\nShutting down. Saving data to JSON file...");
 
     }
 }
